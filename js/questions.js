f18bf4858ee12587452dfc8d041bd6d503282aad
DESKTOP-VIEH4IG\ASUS <trandacphihung120185@gmail.com>
2021-08-13 22:04:29 +0700

    Push code

---
+++ js/questions.js
@@ -0,0 +1,56 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: `Ms. Durkin asked for <a href="#" data-toggle="popover-volunteer" data-original-title="" title="">volunteers</a> to help ------- with the employee <a href="#" data-toggle="popover-fitness">fitness</a> program.`,
+    answer: "(B) her",
+    options: ["(A) she", "(B) her", "(C) hers", "(D) herself"],
+    explain: "abc1",
+  },
+  {
+    numb: 2,
+    question:
+      "Lasner Electronics' staff have extensive ------- of current hardware systems.",
+    answer: "(C) knowledge",
+    options: ["(A) know", "(B) known", "(C) knowledge", "(D) knowledgeable"],
+    explain: "abc2",
+  },
+  {
+    numb: 3,
+    question:
+      "------- a year, Tarrin Industrial Supply audits the accounts of all of its factories",
+    answer: "(A) Once",
+    options: ["(A) Once", "(B) Immediately", "(C) Directly", "(D) Yet"],
+    explain: "abc3",
+  },
+  {
+    numb: 4,
+    question:
+      "Ms. Pham requested a refund ------- the coffeemaker she received was damaged.",
+    answer: "(D) because",
+    options: ["(A) despite", "(B) why", "(C) concerning", "(D) because"],
+    explain: "abc4",
+  },
+  {
+    numb: 5,
+    question:
+      "Information ------- the artwork in the lobby is available at the reception desk.",
+    answer: "(B) about",
+    options: ["(A) across", "(B) about", "(C) upon", "(D) except"],
+    explain: "abc5",
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ js/volcabulary.js
@@ -0,0 +1,20 @@
+let volcab = [
+  {
+    vocab: "volunteer",
+    voice_e:
+      "https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/v/vol/volun/volunteer__gb_1.mp3",
+    voice_a: "Reporter",
+    meaning: "Tình nguyện viên",
+    word_type: "Noun",
+    phonetic: "/ˌvɒlənˈtɪə(r)/",
+  },
+  {
+    vocab: "fitness",
+    voice_e:
+      "https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/f/fit/fitne/fitness__gb_1.mp3",
+    voice_a: "Reporter",
+    meaning: "Thể dục",
+    word_type: "Noun",
+    phonetic: "/ˈfɪtnəs/",
+  },
+];
